598f0adb08021ecc82819d1b097b8efd1fc1f3b8
Shibin Zheng <43055953+es6rc@users.noreply.github.com>
2019-07-13 12:51:29 -0700

    Update augmentations.py

--- albu/augmentations.py
+++ albu/augmentations.py
@@ -5,32 +5,21 @@ import cv2
 def get_training_augmentation(min_area=0., min_visibility=0.):
     train_transform = [
         albu.OneOf([
-            albu.MotionBlur(p=.2),
-            albu.MedianBlur(blur_limit=3, p=0.1),
+            albu.ISONoise(p=.5),
+            albu.GaussNoise(p=0.4),
             albu.Blur(blur_limit=3, p=0.1),
-        ], p=0.2),
-        albu.ShiftScaleRotate(shift_limit=0, scale_limit=0, rotate_limit=15, p=0.5),
+        ]),
         albu.OneOf([
             albu.CLAHE(clip_limit=2),
-            albu.IAASharpen(),
-            albu.IAAEmboss(),
-        ], p=0.3),
+        ], p=0.2),
         albu.OneOf([
-            albu.RandomFog(fog_coef_lower=0.1, fog_coef_upper=0.15, p=0.1),
-            albu.RandomShadow(p=0.1),
-            albu.RandomBrightness(limit=0.3, p=0.2),
-            albu.RandomRain(slant_lower=0, slant_upper=8, drop_length=0, blur_value=4,
-                            brightness_coefficient=0.8, rain_type='heavy', p=0.1),
-            albu.RandomSunFlare(p=0.2),
+            albu.RandomSnow(snow_point_lower=0., snow_point_upper=0.2, brightness_coeff=2., p=0.5),
+            albu.RandomSunFlare(p=0.5),
         ]),
         albu.OneOf([
             albu.RGBShift(p=0.1),
-            albu.HueSaturationValue(p=0.3),
-        ]),
-        albu.OneOf([
-            albu.HorizontalFlip(p=0.5),
-            albu.RandomSizedCrop(min_max_height=(720, 1380), height=1380, width=720, interpolation=cv2.INTER_AREA)
-        ], p=0.2)
+            albu.ChannelShuffle(p=0.2),
+        ])
     ]
     return albu.Compose(train_transform,
                         bbox_params={'format': 'coco', 'min_area': min_area, 'min_visibility': min_visibility,
